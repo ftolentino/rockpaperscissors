2848b81ded03cb7eeb5f4a1513043d0b8b8675b1
ftolentino <filmer.tolentino@gmail.com>
2025-02-20 14:30:53 -0600

    created prompt and getHuman choice function

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ function getComputerChoice() {
   const choiceArr = ["rock", "paper", "scissors"];
 
   let randomChoice = Math.floor(Math.random() * choiceArr.length);
-  console.log(randomChoice);
+  console.log(choiceArr[randomChoice]);
+}
+
+getComputerChoice();
+
+let choice = prompt("What is your choice?");
+
+function getHumanChoice() {
+  console.log("Player choice: ", choice);
 }
 
 getComputerChoice();
