d7db508d5c00967c26261aa445d6e5150eb16f61
ftolentino <filmer.tolentino@gmail.com>
2025-02-20 15:10:56 -0600

    add game play logic, WIP

--- index.js
+++ index.js
@@ -5,12 +5,45 @@ function getComputerChoice() {
   console.log(choiceArr[randomChoice]);
 }
 
-getComputerChoice();
-
-let choice = prompt("What is your choice?");
+let choice = prompt("What is your choice?").toLocaleLowerCase();
 
 function getHumanChoice() {
   console.log("Player choice: ", choice);
 }
 
-getComputerChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playGame() {
+  // player and computer score
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanScore, computerScore) {
+    if (humanSelection === computerSelection) {
+      console.log("It's a tie");
+    } else if (humanSelection === "rock" && computerSelection === "scissors") {
+      console.log("You win!");
+      humanScore++;
+    } else if (humanSelection === "scissors" && computerSelection === "paper") {
+      console.log("You win!");
+      humanScore++;
+    } else if (humanSelection === "paper" && computerSelection === "rock") {
+      console.log("You win!");
+      humanScore++;
+    } else {
+      console.log("You lose!");
+      computerScore++;
+    }
+    console.log("Player score: ", humanScore);
+    console.log("Computer score: ", computerScore);
+  }
+
+  playRound(humanScore, computerScore);
+}
+
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
